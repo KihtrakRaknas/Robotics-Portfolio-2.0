a8d7f28c2bd50a24478fc2ae5b07399b2eb3540e
KihtrakRaknas <karthikandsankar@gmail.com>
2019-04-25 15:53:27 -0400

    Updates

---
+++ precache-manifest.b705ab1e508a32b8d3ff8514c282914a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "27233e62afaf277807bcc2883f081d35",
+    "url": "/Robotics-Portfolio-2.0/static/media/placeholder.27233e62.png"
+  },
+  {
+    "revision": "0616d45f790aef786f6f",
+    "url": "/Robotics-Portfolio-2.0/static/js/runtime~main.5aced26a.js"
+  },
+  {
+    "revision": "d5ed712d65fcb9d5e88d",
+    "url": "/Robotics-Portfolio-2.0/static/js/main.087de0a0.chunk.js"
+  },
+  {
+    "revision": "43e8e585a1bf60b2abc4",
+    "url": "/Robotics-Portfolio-2.0/static/js/2.4f0676d2.chunk.js"
+  },
+  {
+    "revision": "d5ed712d65fcb9d5e88d",
+    "url": "/Robotics-Portfolio-2.0/static/css/main.9ff32b4d.chunk.css"
+  },
+  {
+    "revision": "43e8e585a1bf60b2abc4",
+    "url": "/Robotics-Portfolio-2.0/static/css/2.e4e60d59.chunk.css"
+  },
+  {
+    "revision": "3c525601ea764975d9e0e6b83847a447",
+    "url": "/Robotics-Portfolio-2.0/index.html"
+  }
+];
