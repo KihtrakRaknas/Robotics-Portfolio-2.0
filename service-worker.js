bbf0d0bafa6f8e2af9783614daa8ae807f3879f8
KihtrakRaknas <karthikandsankar@gmail.com>
2019-04-29 00:35:38 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Robotics-Portfolio-2.0/precache-manifest.3e15d2c72a58d3829e5eb126dce5c003.js"
+  "/Robotics-Portfolio-2.0/precache-manifest.011d50b64c9e491f21e50bdc2344e4ec.js"
 );
 
 workbox.clientsClaim();
